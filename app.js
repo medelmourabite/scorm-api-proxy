7a42ac876d4909e8b772e7adec6ec7c1e7949f8a
Mohamed El Mourabite <mohamed@digitalchefs.nl>
2023-08-16 00:19:20 +0100

    added cors

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const { getCompletionStatus, getLaunchLink } = require('./api');
 const app = express();
 const PORT = 3000;
 
-app.use(cors());
+app.use(cors({ origin: ['https://cloud.scorm.com', 'http://127.0.0.1:3001'] }));
 app.use(express.json());
 
 app.get('/', (req, res) => {
